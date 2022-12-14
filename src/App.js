4f07c6b3f6d7e3b6d6816db23968ba8c024d6950
AmitxParmar <amitparmar901@gmail.com>
2022-12-14 14:21:01 +0530

    setup random quote generation, added styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import "./App.css"
 const App = () => {
     const [advice, setAdvice] = useState('')
-    useEffect(() => {
-        console.log('component did update')
-    }, [])
-    const fetchAdvice = () => {
+
+    const fetchAdvice = () => { // generate a random quote when called;
         axios.get('https://api.adviceslip.com/advice')
             .then((response) => {
-                console.log(response);
+                const { advice } = response.data.slip;
+                setAdvice(advice);
+                console.log(advice);
             })
             .catch((e) => {
                 console.log(e.message);
             });
     }
+
+    useEffect(() => {
+        fetchAdvice(); // Fetches a random quote every time the user reloads the page.
+    }, [])
+
     return (
-        <div>App</div>
+        <div className="app">
+            <div className='card'>
+                <h1 className='heading'>
+                    {advice}
+                </h1>
+            </div>
+        </div>
     );
 }
 
